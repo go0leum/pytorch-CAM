da1d0c3237d191f9cb2604d4a44e7ab6973c8a75
go0leum <mijin575@gmail.com>
2023-08-14 12:24:03 +0900

    Update:realtime.py and delete CAM.py

--- realtime.py
+++ realtime.py
@@ -9,7 +9,9 @@ from torchvision import transforms
 from itertools import product
 
 import inception
-import CAM
+from torch.nn import functional as F
+import scipy.signal.windows as signal
+from torch.autograd import Variable
 import os, time
 from datetime import datetime
 
@@ -153,8 +155,6 @@ class Display(threading.Thread):
             self.count+=1
         
         return res_img
-        
-        
 
 class FireCAM(threading.Thread):
     def __init__(self, args):
@@ -162,6 +162,7 @@ class FireCAM(threading.Thread):
         self.args = args
         self.features_blobs = []
         self.classes = {0: 'fire', 1: 'nonfire'}
+        self.cached_2d_windows = dict()
         
     def run(self):
         net = self._getModel()
@@ -179,13 +180,17 @@ class FireCAM(threading.Thread):
                     img = q_cam_in.pop()
                     img_set = self._batch_window(img)
                     
-                    fire_alarm, result_img = CAM.get_cam_window(net, self.features_blobs, img, img_set, self.args.stepSize, self.args.windowSize)
-                    q_cam_out.append(result_img)
+                    fire_alarm, cam_set = self._get_cam_window(net, self.features_blobs, img_set)
+                    CAM = self._merge_window(cam_set, self.args.stepSize, self.args.windowSize)
+                    heatmap = cv2.applyColorMap(CAM, cv2.COLORMAP_TURBO)
+                    
+                    q_cam_out.append(heatmap)
                     q_fire.append(fire_alarm)
                         
                 end = datetime.now()
                 
                 elapsed_time = (end-start)
+                print(elapsed_time)
                 max_sleep = 1 / self.args.camfps
                 
                 if max_sleep > elapsed_time.total_seconds():
@@ -253,9 +258,115 @@ class FireCAM(threading.Thread):
     
     def hook_feature(self, module, input, output):
         self.features_blobs.append(output.data.cpu().numpy())
+    
+    def _return_cam(self, feature_conv, weight_softmax, class_idx):
+        # generate the class activation maps upsample to 256x256
+        size_upsample = (224, 224)
+        bz, nc, h, w = feature_conv.shape
+        output_cam = []
+        for idx in class_idx:
+            cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h*w)))
+            cam = cam.reshape(h, w)
+            cam[cam<0] = 0
+            cam[cam>18.5] = 18.5
+            cam_img = cam / 18.5
+            cam_img = np.uint8(255 * cam)
+            output_cam.append(cv2.resize(cam_img, size_upsample))
+        return output_cam
+    
+    def _get_cam_window(self, net, features_blobs, windows):
+        net.eval()
+        
+        res_list = []
+        res = 0
+        
+        params = list(net.parameters())
+        weight_softmax = np.squeeze(params[-2].data.cpu().numpy())
+        
+        normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+        
+        for i in range(len(windows)):
+            img_tensor = normalize(windows[i])
+            img_variable = Variable(img_tensor).cuda()
+            logit = net(img_variable)
+            h_x = F.softmax(logit, dim=1).data.squeeze()
+            _, idx = h_x.sort(1, True)
+            
+            res += (idx[:,0].size()[0] - int(torch.count_nonzero(idx[:,0])))
+            
+            for batch in range(img_tensor.size()[0]):
+                features_conv = np.expand_dims(features_blobs[i][batch], axis=0)
+                CAMs = self._return_cam(features_conv, weight_softmax, [0])
+                res_list.append(CAMs[0])
+                
+        cam_set = np.stack(res_list)
+        
+        if res < 2:
+            fire_alarm = 1
+        else:
+            fire_alarm = 0
+        
+        return fire_alarm, cam_set
+
+    def _window_2d(self, wnd_sz, power=2):
+        '''
+        Make a 1D window function, then infer and return a 2D window function.
+        Done with an augmentation, and self multiplication with its transpose.
+        Could be generalized to more dimensions.
+        '''
+        # Memoization
+        self.cached_2d_windows = dict()
+        key = "{}_{}".format(wnd_sz, power)
+        if key in self.cached_2d_windows:
+            wnd = self.cached_2d_windows[key]
+        else:
+            intersection = int(wnd_sz/4)
+            wnd_outer = (abs(2*(signal.triang(wnd_sz))) ** power)/2
+            wnd_outer[intersection:-intersection] = 0
+
+            wnd_inner = 1 - (abs(2*(signal.triang(wnd_sz) - 1)) ** power)/2
+            wnd_inner[:intersection] = 0
+            wnd_inner[-intersection:] = 0
+
+            wnd = wnd_inner + wnd_outer
+            wnd = wnd / np.average(wnd)
+            wnd = np.expand_dims(np.expand_dims(wnd, -1), -1)
+            wnd = wnd * wnd.transpose(1, 0, 2)
+            self.cached_2d_windows[key] = wnd
+        return wnd
+
+    def _merge_window(self, cam_set, step_Size, window_size):
+        h = self.args.frame_height+step_Size*2
+        w = self.args.frame_width+step_Size*2
+        
+        weights_each = self._window_2d(window_size, power=2).squeeze()
+        CAM = np.zeros((h, w))
+        divisor = np.zeros((h, w))
+        i=0
+        
+        n_y = (h//step_Size)+(h%step_Size!=0)
+        n_x = (w//step_Size)+(w%step_Size!=0)
+        
+        for y, x in product(range(0, n_y), range(0, n_x)):
+            x_i = x*step_Size
+            y_i = y*step_Size
+            x_f = x_i+window_size
+            y_f = y_i+window_size
+            
+            crop_h, crop_w = CAM[y_i:y_f, x_i:x_f].shape
+            CAM[y_i:y_f, x_i:x_f] += (cam_set[i,0:crop_h,0:crop_w]*weights_each[0:crop_h,0:crop_w])
+            divisor[y_i:y_f,x_i:x_f] += weights_each[0:crop_h,0:crop_w]
+            i+=1
+        
+        CAM = np.uint8(CAM/divisor)
+        CAM = CAM[step_Size:h-step_Size, step_Size:w-step_Size]
+        return CAM
 
 def main(args):
-    os.environ['FIREDETECTION_HOME'] = os.getcwd()
+    os.environ['FIRE_DETECTION_HOME'] = os.getcwd()
     if not os.path.exists(args.db_path):
         os.mkdir(args.db_path)
 
